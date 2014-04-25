81c318862ae02eb52f36b246d60ab4c3719bbfc1
LordRalex <lordralex@gmail.com>
2014-04-25 13:08:45 -0500

    Organize based on logical order Also use 404 for blocked files and send SQL errors to the php error log instead of a local file

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ $klein = new \Klein\Klein();
 
 $blocked = array(
     "/composer.[phar|lock|json:format]",
-    "/pdo-errors.log",
     "/vendor",
     "/klein",
     "/mailgun"
@@ -19,10 +18,9 @@ $blocked = array(
 
 foreach ($blocked as $key => $target):
   $klein->respond($target, function($request, $response, $service, $app) {
-    $response->redirect("/403", 302);
+    $response->redirect("/404", 302);
   });
 endforeach;
-
 
 $klein->respond(function($request, $response, $service, $app) {
   $app->register('db', function() {
@@ -58,65 +56,7 @@ $klein->respond('GET', '/[|index|index.php:page]?', function($request, $response
   $service->render("components/index.phtml", array('action' => 'welcome'));
 });
 
-$klein->respond('/bot', function($request, $response, $service, $app) {
-  if (verifySession($app)) {
-    $service->render("components/index.phtml", array('action' => 'bot'));
-  } else {
-    $response->redirect("/login", 302);
-  }
-});
-
-$klein->respond('POST', '/user/approve/[i:id]', function($request, $response, $service, $app) {
-  if (verifySession($app)) {
-    try {
-      $statement = $app->db->prepare("UPDATE auth SET approved=1 WHERE authkey=?");
-      $statement->execute(array($request->id));
-    } catch (PDOException $ex) {
-      
-    }
-    $response->redirect("/user", 302);
-  } else {
-    $response->redirect("/login", 302);
-  }
-});
-
-$klein->respond('POST', '/user/delete/[i:id]', function($request, $response, $service, $app) {
-  if (verifySession($app)) {
-    try {
-      $statement = $app->db->prepare("DELETE FROM auth WHERE authkey=?");
-      $statement->execute(array($request->id));
-    } catch (PDOException $ex) {
-      
-    }
-    $response->redirect("/user", 302);
-  } else {
-    $response->redirect("/login", 302);
-  }
-});
-
-$klein->respond('/user', function($request, $response, $service, $app) {
-  if (verifySession($app)) {
-    $perms['view'] = checkPermission($app, 'viewuser', 'perms_user');
-    if ($perms['view']) {
-      try {
-        $statement = $app->db->prepare("SELECT authkey as id,username as user,email FROM auth WHERE approved=0 and verified=1");
-        $statement->execute();
-        $accounts = $statement->fetchAll();
-      } catch (PDOException $ex) {
-        $accounts = array();
-      }
-    } else {
-      $accounts = array();
-    }
-    $perms['approve'] = checkPermission($app, 'approveuser', 'perms_user');
-    $perms['delete'] = checkPermission($app, 'deleteuser', 'perms_user');
-    $service->render("components/index.phtml", array('action' => 'user', 'accounts' => $accounts, 'perms' => $perms));
-  } else {
-    $response->redirect("/login", 302);
-  }
-});
-
-$klein->respond('/factoid', function($request, $response, $service, $app) {
+$klein->respond('GET', '/factoid', function($request, $response, $service, $app) {
   $game = $request->param("db");
   try {
     $gameliststatement = $app->db->prepare("SELECT idname,displayname FROM games");
@@ -131,6 +71,7 @@ $klein->respond('/factoid', function($request, $response, $service, $app) {
     }
     $factoids = $statement->fetchAll();
   } catch (PDOException $ex) {
+    error_log(addSlashes($ex->getMessage()) . "\r");
     $factoids = array();
     $gamelist = array();
   }
@@ -139,23 +80,15 @@ $klein->respond('/factoid', function($request, $response, $service, $app) {
   $service->render("components/index.phtml", array('action' => 'factoid', 'factoids' => $factoids, 'perms' => $perms, 'game' => $game, 'gamelist' => $gamelist));
 });
 
-$klein->respond('/ban', function($request, $response, $service, $app) {
-  if (verifySession($app)) {
-    $service->render("components/index.phtml", array('action' => 'ban'));
-  } else {
-    $response->redirect("/login", 302);
-  }
-});
-
-$klein->respond('/logout', function($request, $response, $service, $app) {
+$klein->respond('GET', '/logout', function($request, $response, $service, $app) {
   if (!verifySession($app)) {
     $response->redirect("/", 302);
   }
   try {
     $statement = $app->db->prepare("UPDATE auth SET session = ? WHERE authkey = ?");
     $statement->execute(array('null', $_SESSION['authkey']));
-  } catch (PDOException $e) {
-    file_put_contents('../pdo-errors.log', $e->getMessage() . "\n", FILE_APPEND);
+  } catch (PDOException $ex) {
+    error_log(addSlashes($ex->getMessage()) . "\r");
   }
   $_SESSION['authkey'] = null;
   $_SESSION['session'] = null;
@@ -170,79 +103,37 @@ $klein->respond('GET', '/login', function($request, $response, $service, $app) {
   $service->render("components/index.phtml", array('action' => 'login'));
 });
 
-$klein->respond('POST', '/login', function($request, $response, $service, $app) {
-  $service->validateParam('email', 'Please enter a valid eamail')->isLen(5, 256);
-  $service->validateParam('password', 'Please enter a password')->isLen(1, 256);
-  try {
-    $statement = $app->db->prepare("SELECT authkey,password,approved,verified,email FROM auth WHERE email=?");
-    $statement->execute(array($request->param("email")));
-    $statement->setFetchMode(PDO::FETCH_ASSOC);
-    $db = $statement->fetch();
-    if (!isset($db['password']) || !isset($db['authkey']) || !isset($db['approved']) || !isset($db['email'])) {
-      throw new Exception("No user found");
-    }
-    if (password_verify($request->param('password'), $db['password'])) {
-      if ($db['verified'] == 0) {
-        throw new Exception("Your email has not been verified");
-      }
-      if ($db['approved'] == 0) {
-        throw new Exception("Your account has not been approved");
-      }
-      $str = generate_string(64);
-      $statement = $app->db->prepare("UPDATE auth SET session = ? WHERE authkey = ?");
-      $statement->execute(array($str, $db['authkey']));
-      $_SESSION['authkey'] = $db['authkey'];
-      $_SESSION['session'] = $str;
-      $service->back();
-    } else {
-      throw new Exception("Incorrect password");
-    }
-  } catch (PDOException $e) {
-    file_put_contents('../pdo-errors.log', $e->getMessage() . "\n", FILE_APPEND);
-    throw new Exception("The MySQL connection has failed, please contact the admins");
+$klein->respond('GET', '/register', function($request, $response, $service, $app) {
+  if (!verifySession($app)) {
+    $service->render("components/index.phtml", array('action' => 'register'));
+  } else {
+    $response->redirect("/index", 302);
   }
 });
 
-$klein->respond('/settings', function($request, $response, $service, $app) {
-  if (verifySession($app)) {
-    $service->render("components/index.phtml", array('action' => 'settings'));
-  } else {
+$klein->respond('GET', '/verify', function($request, $response, $service, $app) {
+  try {
+    $service->validateParam('email', 'Invalid email')->isLen(5, 256);
+    $service->validateParam('key', 'Invalid verify key')->isLen(32);
+    $statement = $app->db->prepare("SELECT data FROM auth WHERE email=?");
+    $statement->execute(array($request->param("email")));
+    $db = $statement->fetch();
+    if ($request->param('key') == $db['data']) {
+      $statement = $app->db->prepare("UPDATE auth SET verified = 1, data = null WHERE email=?");
+      $statement->execute(array($request->param('email')));
+      $service->flash('Your email has been verified');
+      $response->redirect("/login", 302);
+    } else {
+      throw new Exception("Invalid verify key for the email");
+    }
+  } catch (Exception $e) {
+    $service->flash('Error: ' . $e->getMessage());
     $response->redirect("/login", 302);
   }
 });
 
 $klein->respond('GET', '/resetpw', function($request, $response, $service, $app) {
   $service->render("components/index.phtml", array('action' => 'resetpw'));
-});
-
-$klein->respond('POST', '/resetpw', function($request, $response, $service, $app) {
-  $service->validateParam('email', 'Invalid email')->isLen(5, 256);
-  try {
-    $statement = $app->db->prepare("SELECT authkey,username,email,verified FROM auth WHERE email=?");
-    $statement->execute(array($request->param('email')));
-    $db = $statement->fetch();
-    if (!isset($db['email'])) {
-      $service->flash("DB: " . $db['authkey']);
-      throw new Exception("No user " . $request->param('email') . " found");
-    }
-    if (!isset($db['verified']) || $db['verified'] == 0) {
-      throw new Exception("Account " . $request->param('email') . " not verified");
-    }
-    $authkey = $db['authkey'];
-    $resetkey = generate_string(64);
-    $app->db->prepare("UPDATE auth SET data = ? WHERE authkey = ?")->execute(array($resetkey, $authkey));
-    $url = $app->fullsite . '/reset-pw?authkey=' . $authkey . '&resetkey=' . $resetkey;
-    $app->mail->sendMessage($app->domain, array('from' => 'Noreply <' . $app->email . '>',
-        'to' => $db['email'],
-        'subject' => 'Password reset for cp.ae97.net',
-        'html' => 'Someone requested your password to be reset. If you wanted to do this, please use <strong><a href="' . $url . '">this link</a></strong> to '
-        . 'reset your password'));
-    $service->flash('Your reset link has been emailed to you');
-    $response->redirect('/login', 302);
-  } catch (PDOException $e) {
-    file_put_contents('../pdo-errors.log', $e->getMessage() . "\n", FILE_APPEND);
-    throw new Exception("The MySQL connection has failed, please contact the admins");
-  }
 });
 
 $klein->respond('GET', '/reset-pw', function($request, $response, $service, $app) {
@@ -269,26 +160,127 @@ $klein->respond('GET', '/reset-pw', function($request, $response, $service, $app
       $app->db->prepare("UPDATE auth SET password = ?, data = null WHERE authkey = ?")->execute(array($newpass, $db['authkey']));
       $app->mail->sendMessage($app->domain, array('from' => 'Noreply <' . $app->email . '>',
           'to' => $db['email'],
-          'subject' => 'New panel password',
-          'html' => 'Your password has been changed. Your new password is : ' . $unhashed));
+          'subject' => 'New panel password', 'html' => 'Your password has been changed. Your new password is : ' . $unhashed));
       $service->flash('Your new password has been emailed to you');
       $response->redirect('/login', 302);
     } else {
       $service->flash("Error: Reset key has expired");
       $response->redirect('/resetpw', 302);
     }
-  } catch (PDOException $e) {
-    file_put_contents('../pdo-errors.log', $e->getMessage() . "\n", FILE_APPEND);
+  } catch (PDOException $ex) {
+    error_log(addSlashes($ex->getMessage()) . "\r");
     $service->flash("Error: The MySQL connection has failed, please contact the admins");
     $response->redirect('/login', 302);
   }
 });
 
-$klein->respond('GET', '/register', function($request, $response, $service, $app) {
-  if (!verifySession($app)) {
-    $service->render("components/index.phtml", array('action' => 'register'));
+$klein->respond('GET', '/settings', function($request, $response, $service, $app) {
+  if (verifySession($app)) {
+    $service->render("components/index.phtml", array('action' => 'settings'));
   } else {
-    $response->redirect("/index", 302);
+    $response->redirect("/login", 302);
+  }
+});
+
+$klein->respond('GET', '/bot', function($request, $response, $service, $app) {
+  if (verifySession($app)) {
+    $service->render("components/index.phtml", array('action' => 'bot'));
+  } else {
+    $response->redirect("/login", 302);
+  }
+});
+
+$klein->respond('GET', '/user', function($request, $response, $service, $app) {
+  if (verifySession($app)) {
+    $perms['view'] = checkPermission($app, 'viewuser', 'perms_user');
+    if ($perms['view']) {
+      try {
+        $statement = $app->db->prepare("SELECT authkey as id,username as user,email FROM auth WHERE approved=0 and verified=1");
+        $statement->execute();
+        $accounts = $statement->fetchAll();
+      } catch (PDOException $ex) {
+        error_log(addSlashes($ex->getMessage()) . "\r");
+        $accounts = array();
+      }
+    } else {
+      $accounts = array();
+    }
+    $perms['approve'] = checkPermission($app, 'approveuser', 'perms_user');
+    $perms['delete'] = checkPermission($app, 'deleteuser', 'perms_user');
+    $service->render("components/index.phtml", array('action' => 'user', 'accounts' => $accounts, 'perms' => $perms));
+  } else {
+    $response->redirect("/login", 302);
+  }
+});
+
+$klein->respond('GET', '/ban', function ($request, $response, $service, $app) {
+  if (verifySession($app)) {
+    $service->render("components/index.phtml", array('action' => 'ban'));
+  } else {
+    $response->redirect("/login", 302);
+  }
+});
+
+$klein->respond('POST', '/login', function ( $request, $response, $service, $app) {
+  $service->validateParam('email', 'Please enter a valid eamail')->isLen(5, 256);
+  $service->validateParam('password', 'Please enter a password')->isLen(1, 256);
+  try {
+    $statement = $app->db->prepare("SELECT authkey,password,approved,verified,email FROM auth WHERE email=?");
+    $statement->execute(array($request->param("email")));
+    $statement->setFetchMode(PDO::FETCH_ASSOC);
+    $db = $statement->fetch();
+    if (!isset($db['password']) || !isset($db['authkey']) || !isset($db['approved']) || !isset($db['email'])) {
+      throw new Exception("No user found");
+    }
+    if (password_verify($request->param('password'), $db['password'])) {
+      if ($db['verified'] == 0) {
+        throw new Exception("Your email has not been verified");
+      }
+      if ($db['approved'] == 0) {
+        throw new Exception("Your account has not been approved");
+      }
+      $str = generate_string(64);
+      $statement = $app->db->prepare("UPDATE auth SET session = ? WHERE authkey = ?");
+      $statement->execute(array($str, $db['authkey']));
+      $_SESSION['authkey'] = $db['authkey'];
+      $_SESSION['session'] = $str;
+      $service->back();
+    } else {
+      throw new Exception("Incorrect password");
+    }
+  } catch (PDOException $ex) {
+    error_log(addSlashes($ex->getMessage()) . "\r");
+    throw new Exception("The MySQL connection has failed, please contact the admins");
+  }
+});
+
+
+$klein->respond('POST', '/resetpw', function($request, $response, $service, $app) {
+  $service->validateParam('email', 'Invalid email')->isLen(5, 256);
+  try {
+    $statement = $app->db->prepare("SELECT authkey,username,email,verified FROM auth WHERE email=?");
+    $statement->execute(array($request->param('email')));
+    $db = $statement->fetch();
+    if (!isset($db['email'])) {
+      throw new Exception("No user " . $request->param('email') . " found");
+    }
+    if (!isset($db['verified']) || $db['verified'] == 0) {
+      throw new Exception("Account " . $request->param('email') . " not verified");
+    }
+    $authkey = $db['authkey'];
+    $resetkey = generate_string(64);
+    $app->db->prepare("UPDATE auth SET data = ? WHERE authkey = ?")->execute(array($resetkey, $authkey));
+    $url = $app->fullsite . '/reset-pw?authkey=' . $authkey . '&resetkey=' . $resetkey;
+    $app->mail->sendMessage($app->domain, array('from' => 'Noreply <' . $app->email . '>',
+        'to' => $db['email'],
+        'subject' => 'Password reset for cp.ae97.net',
+        'html' => 'Someone requested your password to be reset. If you wanted to do this, please use <strong><a href="' . $url . '">this link</a></strong> to '
+        . 'reset your password'));
+    $service->flash('Your reset link has been emailed to you');
+    $response->redirect('/login', 302);
+  } catch (PDOException $ex) {
+    error_log(addSlashes($ex->getMessage()) . "\r");
+    throw new Exception("The MySQL connection has failed, please contact the admins");
   }
 });
 
@@ -334,6 +326,7 @@ $klein->respond('POST', '/register', function($request, $response, $service, $ap
       $approveKey = generate_string(32);
       $hashedPW = password_hash($request->param('password'), PASSWORD_DEFAULT);
       $params = array($request->param('username'), $request->param('email'), $hashedPW, 0, 0, $approveKey);
+
       $statement->execute($params);
       $app->mail->sendMessage($app->domain, array('from' => 'Noreply <' . $app->email . '>',
           'to' => $request->param('email'),
@@ -342,31 +335,39 @@ $klein->respond('POST', '/register', function($request, $response, $service, $ap
           . 'If this was you, please click the following link to verify your email: <a href="' . $app->fullsite . '/verify?email=' . $request->param("email") . '&key=' . $approveKey . '">Verify email</a>'));
       $service->flash("Your account has been created, an email has been sent to verify");
       $response->redirect("/login", 302);
-    } catch (PDOException $e) {
-      file_put_contents('../pdo-errors.log', $e->getMessage() . "\n", FILE_APPEND);
+    } catch (PDOException $ex) {
+      error_log(addSlashes($ex->getMessage()) . "\r");
       $service->flash("Error: The MySQL connection has failed, please contact the admins");
       $response->redirect('/register', 302);
     }
   }
 });
 
-$klein->respond('GET', '/verify', function($request, $response, $service, $app) {
-  try {
-    $service->validateParam('email', 'Invalid email')->isLen(5, 256);
-    $service->validateParam('key', 'Invalid verify key')->isLen(32);
-    $statement = $app->db->prepare("SELECT data FROM auth WHERE email=?");
-    $statement->execute(array($request->param("email")));
-    $db = $statement->fetch();
-    if ($request->param('key') == $db['data']) {
-      $statement = $app->db->prepare("UPDATE auth SET verified = 1, data = null WHERE email=?");
-      $statement->execute(array($request->param('email')));
-      $service->flash('Your email has been verified');
-      $response->redirect("/login", 302);
-    } else {
-      throw new Exception("Invalid verify key for the email");
+$klein->respond('POST', '/user/approve/[i:id]', function($request, $response, $service, $app) {
+  if (verifySession($app)) {
+    try {
+      $statement = $app->db->prepare("UPDATE auth SET approved=1 WHERE authkey=?");
+      $statement->execute(array($request->id));
+    } catch (PDOException $ex) {
+      error_log(addSlashes($ex->getMessage()) . "\r");
     }
-  } catch (Exception $e) {
-    $service->flash('Error: ' . $e->getMessage());
+    $response->redirect("/user", 302);
+  } else {
+    $response->redirect("/login", 302);
+  }
+});
+
+$klein->respond('POST', '/user/delete/[i:id]', function($request, $response, $service, $app) {
+  if (verifySession($app)) {
+    try {
+      $statement = $app->db->prepare("DELETE FROM auth WHERE authkey=?");
+      $statement->execute(array($request->
+          id));
+    } catch (PDOException $ex) {
+      error_log(addSlashes($ex->getMessage()) . "\r");
+    }
+    $response->redirect("/user", 302);
+  } else {
     $response->redirect("/login", 302);
   }
 });
@@ -403,8 +404,8 @@ function verifySession($app) {
       $_SESSION['session'] = null;
       return false;
     }
-  } catch (PDOException $e) {
-    file_put_contents('../pdo-errors.log', $e->getMessage() . "\n", FILE_APPEND);
+  } catch (PDOException $ex) {
+    error_log(addSlashes($ex->getMessage()) . "\r");
     $_SESSION['authkey'] = null;
     $_SESSION['session'] = null;
     return false;
@@ -422,6 +423,7 @@ function checkPermission($app, $perm, $table) {
     $db = $statement->fetch();
     return isset($db[$perm]) && $db[$perm] === "1";
   } catch (PDOException $ex) {
+    error_log(addSlashes($ex->getMessage()) . "\r");
     return 0;
   }
 }
