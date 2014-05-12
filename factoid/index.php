9647c34c96d81436a87b8b3f30fbfe410045600c
LordRalex <lordralex@gmail.com>
2014-05-11 21:39:24 -0500

    Start working on getting jquery to gather data

--- factoid/index.php
+++ factoid/index.php
@@ -1,27 +1,10 @@
 <?php
-
 $this->respond('GET', '/[|index|index.php:page]?', function($request, $response, $service, $app) {
   $game = $request->param("db");
-  try {
-    $gameliststatement = $app->db->prepare("SELECT idname,displayname FROM games");
-    $gameliststatement->execute();
-    $gamelist = $gameliststatement->fetchAll();
-    if ($game == null) {
-      $statement = $app->db->prepare("SELECT id,name,content,game FROM factoids");
-      $statement->execute();
-    } else {
-      $statement = $app->db->prepare("SELECT id,name,content FROM factoids WHERE game=?");
-      $statement->execute(array($game));
-    }
-    $factoids = $statement->fetchAll();
-  } catch (PDOException $ex) {
-    error_log(addSlashes($ex->getMessage()) . "\r");
-    $factoids = array();
-    $gamelist = array();
-  }
+
   $perms['edit'] = checkPermission($app, 'editentry', 'perms_factoid');
   $perms['delete'] = checkPermission($app, 'removeentry', 'perms_factoid');
-  $service->render('index.phtml', array('action' => 'factoid', 'page' => 'factoid/factoid.phtml', 'factoids' => $factoids, 'perms' => $perms, 'game' => $game, 'gamelist' => $gamelist));
+  $service->render('index.phtml', array('action' => 'factoid', 'page' => 'factoid/factoid.phtml', 'perms' => $perms));
 });
 
 $this->respond('POST', '/delete', function($request, $response, $service, $app) {
@@ -60,19 +43,40 @@ $this->respond('POST', '/new', function($request, $response, $service, $app) {
   }
 });
 
-$this->respond('GET', '/get', function($request, $response, $service, $app) {
-  include('post.php');
-  echo getDatabase($app->db, $request->param("db"));
-});
-
-$this->respond('POST', '/get', function($request, $response, $service, $app) {
-  if (verifySession($app)) {
-    try {
-      
-    } catch (PDOException $ex) {
-      error_log(addSlashes($ex->getMessage()) . "\r");
+$this->respond('/get', function($request, $response, $service, $app) {
+  $game = $request->param('db');
+  $database = $app->db;
+  try {
+    $gameliststatement = $database->prepare("SELECT idname,displayname FROM games");
+    $gameliststatement->execute();
+    $gamelist = $gameliststatement->fetchAll();
+    if ($game == null) {
+      $statement = $database->prepare("SELECT id,name,content,game FROM factoids");
+      $statement->execute();
+    } else {
+      $statement = $database->prepare("SELECT id,name,content FROM factoids WHERE game=?");
+      $statement->execute(array($game));
     }
-  } else {
-    $response->redirect("/auth/login", 302);
+    $factoids = $statement->fetchAll();
+  } catch (PDOException $ex) {
+    error_log(addSlashes($ex->getMessage()) . "\r");
+    $factoids = array();
+    $gamelist = array();
   }
+  $compiledGamelist = array();
+  $counter = 0;
+  foreach ($gamelist as $gameitem):
+    $compiledGamelist[$counter] = array('idname' => $gameitem['idname'], 'displayname' => $gameitem['displayname']);
+    $counter++;
+  endforeach;
+  $compiledFactoidlist = array();
+  $counter = 0;
+  foreach ($factoids as $f):
+    $compiledFactoidlist[$counter] = array('id' => $f['id'], 'name' => $f['name'], 'content' => $f['content'], 'game' => $game == null ? $f['name'] : $game);
+    $counter++;
+  endforeach;
+  $collection = array();
+  $collection['games'] = $compiledGamelist;
+  $collection['factoids'] = $compiledFactoidlist;
+  echo json_encode($collection);
 });
