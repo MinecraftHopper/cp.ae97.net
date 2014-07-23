16ab57f52d5a1d270b8375ffca4381660392b69e
Lord_Ralex <lordralex@gmail.com>
2014-07-22 21:16:32 -0500

    Add default config

---
+++ config/defaultconfig.php
@@ -0,0 +1,26 @@
+<?php
+
+function getDatabaseConfig() {
+    return [
+        'host' => 'localhost',
+        'factoiddb' => 'factoid',
+        'authdb' => 'authentication',
+        'user' => '',
+        'pass' => ''
+    ];
+}
+
+function getMailgunConfig() {
+    return [
+        'key' => '',
+        'email' => 'noreply@example.com'
+    ];
+}
+
+function getSiteConfig() {
+    return [
+        'domain' => 'example.com',
+        'site' => 'cp.example.com',
+        'fullsite' => 'http://cp.example.com'
+    ];
+}
