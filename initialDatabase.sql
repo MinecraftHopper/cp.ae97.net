73a02f8799e794c30fcb784b6a7a1ab6d94b2766
Joshua Taylor <lordralex@gmail.com>
2015-01-14 23:45:54 -0600

    Fix creation order

--- initialDatabase.sql
+++ initialDatabase.sql
@@ -2,12 +2,29 @@ DROP DATABASE IF EXISTS `panel`;
 CREATE DATABASE IF NOT EXISTS `panel`;
 USE `panel`;
 
-DROP TABLE IF EXISTS `banchannels`;
-CREATE TABLE IF NOT EXISTS `banchannels` (
-  `banId` int(10) unsigned NOT NULL,
-  `channel` varchar(64) NOT NULL,
-  PRIMARY KEY (`banId`,`channel`),
-  CONSTRAINT `FK_banchannels_bans` FOREIGN KEY (`banId`) REFERENCES `bans` (`id`)
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `uuid` varchar(36) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `verified` tinyint(1) NOT NULL DEFAULT '0',
+  `approved` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY `unique_email` (`email`),
+  UNIQUE KEY `unique_username` (`username`),
+  KEY `username` (`username`),
+  KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `games`;
+CREATE TABLE IF NOT EXISTS `games` (
+  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `idname` varchar(255) NOT NULL,
+  `displayname` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idname` (`idname`),
+  UNIQUE KEY `displayname` (`displayname`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `bans`;
@@ -25,6 +42,14 @@ CREATE TABLE IF NOT EXISTS `bans` (
   CONSTRAINT `FK_bans_users` FOREIGN KEY (`issuedBy`) REFERENCES `users` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `banchannels`;
+CREATE TABLE IF NOT EXISTS `banchannels` (
+  `banId` int(10) unsigned NOT NULL,
+  `channel` varchar(64) NOT NULL,
+  PRIMARY KEY (`banId`,`channel`),
+  CONSTRAINT `FK_banchannels_bans` FOREIGN KEY (`banId`) REFERENCES `bans` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 DROP TABLE IF EXISTS `factoids`;
 CREATE TABLE IF NOT EXISTS `factoids` (
   `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
@@ -36,16 +61,6 @@ CREATE TABLE IF NOT EXISTS `factoids` (
   KEY `FK_factoids_games` (`game`),
   KEY `key_id` (`id`),
   CONSTRAINT `FK_factoids_games` FOREIGN KEY (`game`) REFERENCES `games` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `games`;
-CREATE TABLE IF NOT EXISTS `games` (
-  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
-  `idname` varchar(255) NOT NULL,
-  `displayname` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `idname` (`idname`),
-  UNIQUE KEY `displayname` (`displayname`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `passwordreset`;
@@ -64,14 +79,6 @@ CREATE TABLE IF NOT EXISTS `permissions` (
   UNIQUE KEY `perm` (`perm`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `session`;
-CREATE TABLE IF NOT EXISTS `session` (
-  `uuid` varchar(36) NOT NULL,
-  `sessionToken` varchar(256) NOT NULL,
-  PRIMARY KEY (`uuid`),
-  CONSTRAINT `FK_session_users` FOREIGN KEY (`uuid`) REFERENCES `users` (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 DROP TABLE IF EXISTS `userperms`;
 CREATE TABLE IF NOT EXISTS `userperms` (
   `userid` varchar(36) NOT NULL,
@@ -82,19 +89,12 @@ CREATE TABLE IF NOT EXISTS `userperms` (
   CONSTRAINT `FK_userperms_users` FOREIGN KEY (`userid`) REFERENCES `users` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
+DROP TABLE IF EXISTS `session`;
+CREATE TABLE IF NOT EXISTS `session` (
   `uuid` varchar(36) NOT NULL,
-  `username` varchar(64) NOT NULL,
-  `email` varchar(64) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `verified` tinyint(1) NOT NULL DEFAULT '0',
-  `approved` tinyint(1) NOT NULL DEFAULT '0',
+  `sessionToken` varchar(256) NOT NULL,
   PRIMARY KEY (`uuid`),
-  UNIQUE KEY `unique_email` (`email`),
-  UNIQUE KEY `unique_username` (`username`),
-  KEY `username` (`username`),
-  KEY `email` (`email`)
+  CONSTRAINT `FK_session_users` FOREIGN KEY (`uuid`) REFERENCES `users` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `verification`;
