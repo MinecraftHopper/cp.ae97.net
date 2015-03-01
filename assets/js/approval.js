b3587a8d5731e9dc8a55ad0364c478d6b0d9e3c4
Lord_Ralex <lordralex@gmail.com>
2015-03-01 01:04:52 -0600

    I missed this path

--- assets/js/approval.js
+++ assets/js/approval.js
@@ -1,12 +1,12 @@
 function approveUser(data) {
     var value = data.id;
-    var posting = $.post('/cp/admin/user/approve/' + value, {}, "json");
+    var posting = $.post('/admin/user/approve/' + value, {}, "json");
     posting.done(getUnapprovedUserList);
 }
 
 function getUnapprovedUserList() {
     $("#unapprovedTable").hide();
-    var posting = $.post('/cp/admin/user/list/unapproved', {}, "json");
+    var posting = $.post('/admin/user/list/unapproved', {}, "json");
     posting.done(function(data) {
         var json = JSON.parse(data);
         viewModel.unapprovedUsers.removeAll();
