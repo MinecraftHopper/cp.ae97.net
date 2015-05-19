0b7aad9dde2efdf2a2ecccde9ad9a050527b9c70
Lord_Ralex <lordralex@gmail.com>
2015-05-19 11:36:20 -0500

    Update upstream

--- tools/updatefactoids.php
+++ tools/updatefactoids.php
@@ -11,7 +11,7 @@ $_DATABASE = array(
 
 $database = new PDO("mysql:host=" . $_DATABASE['host'] . ";dbname=" . $_DATABASE['db'], $_DATABASE['user'], $_DATABASE['pass'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 
-$factoids = file_get_contents("http://vps.dope.ghoti.me/mcfaq/faqdatabase");
+$factoids = file_get_contents("http://home.ghoti.me/~faqbot/faqdatabase");
 
 $list = explode("\n", $factoids);
 
@@ -19,15 +19,16 @@ foreach ($list as $line) {
     if (!trim($line)) {
         continue;
     }
+    echo "Importing: " . $line . "\n";
     list($name, $context) = explode('|', $line, 2);
     $set = $database->prepare("SELECT game FROM factoids WHERE name = ?");
     $set->execute(array($name));
     $record = $set->fetch();
     if (isset($record['game']) && $record['game'] == 0) {
         $database->prepare("UPDATE factoids SET content = ? WHERE game = 0 AND name = ?")
-                ->execute(array($context, $name));
+              ->execute(array($context, $name));
     } else {
         $database->prepare("INSERT INTO factoids (name, game, content) VALUES (?, 1, ?) ON DUPLICATE KEY UPDATE content = ?")
-                ->execute(array($name, $context, $context));
+              ->execute(array($name, $context, $context));
     }
 }
