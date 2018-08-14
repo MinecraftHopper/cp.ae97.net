d1be7562444f64a6184ff323d150015d8c620067
Uriel Salischiker <usalischiker@hexacta.com>
2018-08-14 08:45:14 -0300

    Fix HJT

--- functions/autoload.php
+++ functions/autoload.php
@@ -2,7 +2,8 @@
 
 require_once 'panel/config.php';
 require_once 'panel/authentication.php';
-require_once 'panel//utilities.php';
+require_once 'panel/utilities.php';
+require_once 'panel/hjt.php';
 require_once 'panel/factoids.php';
 require_once 'panel/email.php';
 require_once 'panel/bans.php';
